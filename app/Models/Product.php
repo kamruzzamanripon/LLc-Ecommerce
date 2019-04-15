79ca34499a2b77c5fef31cecc1e1cf5b96427a88
syedkamruzzaman <rkamruzzaman@gmail.com>
2019-04-15 20:28:45 +0600

    migration and seeder update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
 class Product extends Model implements HasMedia
 {
